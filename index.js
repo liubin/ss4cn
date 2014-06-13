87614c0af5be5cb37f250cc2312e33ddd55d9a1b
bin liu <liubin0329@gmail.com>
2014-06-13 12:45:11 +0800

    scaffold complete

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 var express = require("express");
 var app = express();
+var ejs = require('ejs');
+var bodyParser = require('body-parser')
 
+app.set('views', __dirname + '/views');
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'hehe!');
+});
+
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+
+app.get('/www.slideshare.net/:user/:presentation', function(req, res) {
+  url = "http://www.slideshare.net/" + req.params.user
+                                      + "/" + req.params.presentation;
+
+  res.render('index', {
+    title: url
+  });
 });
 
 var port = Number(process.env.PORT || 5000);
